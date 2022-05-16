00e06661a90cbadf582b30e11821eaf31afe00ba
gomin0 <>
2022-05-16 05:11:16 +0000

    other

---
+++ algorithm/other_algotirhm/algo02.py
@@ -0,0 +1,15 @@
+#소수 판변 함수(제곱근)
+
+import math
+
+#소수 판별 함수
+def is_prime_number(x):
+    #2부터 x의 제곱근까지의 모든 수를 확인하며
+    for i in range(2, int(math.sqrt(x)) + 1):
+        #x가 해당 수로 나눠떨어지면
+        if x % i == 0:
+            return False #소수가 아님
+    return True #소수임
+
+print(is_prime_number(4))
+print(is_prime_number(7))
---
+++ algorithm/other_algotirhm/algo07.py
@@ -0,0 +1,7 @@
+#순열
+import itertools
+
+data = [1, 2]
+
+for x in itertools.permutations(data, 2):
+    print(list(x))
